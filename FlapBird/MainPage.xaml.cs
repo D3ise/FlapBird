e3f3415eb3bc4f1e5db2322b72bbd85c9e33c6a4
deise <deiseelen17@gmail.com>
2024-10-10 09:00:58 -0300

    SALVO!!!

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -2,12 +2,32 @@
 
 public partial class MainPage : ContentPage
 {
+	const int  gravidade=3;
+	const int tempoEntreFrames=10;
+	bool morto=false;
 	
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		Desenhar();
+    }
 
+    async Task Desenhar()
+	{
+		while (!morto)
+		{
+			AplicarGravidade();
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+ void AplicarGravidade ()
+ {
+	pas.TranslationY+=gravidade;
+ }
 	
 }
 
